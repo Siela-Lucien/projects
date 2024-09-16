f74df0f57e4cf5e2b8e2b8ba0166aaf3e09d3a82
Lucien <siellucien2024@gmail.com>
2024-09-17 00:46:53 +0100

    grace

--- src/app/tab1/tab1.page.ts
+++ src/app/tab1/tab1.page.ts
@@ -1,12 +1,49 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { Variables } from '../services/variables.service';
+import { StorageService } from '../services/storage.service';
+import { DataService } from '../services/data.service';
+import { LoginService } from '../services/login.service';
 
 @Component({
   selector: 'app-tab1',
   templateUrl: 'tab1.page.html',
-  styleUrls: ['tab1.page.scss']
+  styleUrls: ['tab1.page.scss'],
 })
-export class Tab1Page {
+export class Tab1Page implements OnInit {
+  pageTitle = this.variables.appId;
+  heroes: any = [];
 
-  constructor() {}
+  constructor(
+    public variables: Variables,
+    private storage: StorageService,
+    private _data: DataService,
+    private _login: LoginService
+  ) {
+    variables.appId = 'App ID Changed';
+  }
+  ngOnInit(): void {
+    const data = {
+      route: 'get-heroes',
+    };
 
+    this._data.get_data(this.variables.object_to_query(data)).subscribe(
+      (resp) => {
+        if (resp && 'heroes' in resp) {
+          this.heroes = resp.heroes;
+          this.storage.set('heroes', this.heroes);
+        } else {
+          this._data.handle_server_err();
+        }
+      },
+      (err) => {
+        this.storage.get('heroes').then((val) => {
+          if (val) {
+            this.heroes = val;
+          } else {
+            this._data.handle_failed_conn();
+          }
+        });
+      }
+    );
+  }
 }
